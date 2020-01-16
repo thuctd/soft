b5feafb416083d8a23f5c0be7e4bf856b343c686
ThucTran <thucspc@gmail.com>
2020-01-17 05:22:46 +0700

    ok

--- GitPush.bat
+++ GitPush.bat
@@ -1,4 +1,4 @@
-call git pull
+call git pull origin master
 call git add .
 call git commit -am "ok"
 call git push origin master
---
+++ gitConfig.bat
@@ -0,0 +1,2 @@
+call git config user.email "thucspc@gmail.com"
+call git config user.name "ThucTran"
